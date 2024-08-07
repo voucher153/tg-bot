571a492e891858fb5f4514bda8d354b39190203c
IdelRamazanov <vouchercomm227@gmail.com>
2024-08-07 15:46:14 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Cookies from "js-cookie"
 import './App.css';
 
 const tg = window.Telegram.WebApp
@@ -13,9 +14,14 @@ function App() {
     tg.close()
   }
 
+  const cookSm = () => {
+    Cookies.set('test', 'works')
+  }
+
   return (
     <div>
       Hello
+      <button onClick={cookSm}>Cook</button>
       <button onClick={onClose}>Close</button>
     </div>
   );
